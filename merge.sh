58d4d05cc63dc14c2bc30e21e2da424671abb047
OKUGAWA Yuichi <yokugawa@cw13.cc.kek.jp>
2019-12-16 20:20:25 +0900

    update 12/16/19

--- merge.sh
+++ merge.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-#bfaf=$1
+if [ -z "$1" ] || [ -z "$2" ]; then
+	echo "Please enter isolated lepton type."
+	exit 1
+fi
 
-#if [ -z $bfaf ] ; then
-#  hadd -f root_merge/hadronic_eLpR_kekcc_500k.root rootfile/kekcc/rv01-16-p05_500*I108679*.root rootfile/kekcc/rv01-16-p05_500*I108681*.root
-#else
-#  hadd -f root_merge/aftervertexrestore_hadronic_eLpR_kekcc_500k.root rootfile/kekcc/aftervertexrestore*I108679*.root rootfile/kekcc/aftervertexrestore*I108681*.root
-#fi
+model=$1
+isolep=$2
+dir=/hsm/ilc/users/yokugawa/preset_N_run/${model}/${isolep}
 
-#hadd -f root_merge/new/large/leptonic_yyxyev_eLeR_new_large_QQbar_NewIsoLep_030619.root rootfile/rv02*mILD_l5_o1_v02*I108670*NewVtx*
-hadd -f root_merge/new/small/QQbar_s5_yyxyev_eLeR.root rootfile/rv02*mILD_s5_o1_v02*I108670*NewVtx*
-
-# yyxylv
-#hadd -f root_merge/new/large/QQbar_l5_yyxylv_eLeR.root rootfile/rv02*mILD_l5_o1_v02*I108675*NewVtx*
-#hadd -f root_merge/new/small/QQbar_s5_yyxylv_eLeR.root rootfile/rv02*mILD_s5_o1_v02*I108675*NewVtx*
+if [ ! -d "$dir" ]; then
+	echo "The directory does not exist."
+	exit 1
+else
+	echo ${dir}
+	hadd -f ${dir}/QQbarProcessor_out/root_merge/IsoLepTagged.eL.pR_${isolep}_QQbar.root ${dir}/QQbarProcessor_out/*.root
+#	hadd -f ${dir}/TrashRecoProcessor_out/before_vtx_recovery/root_merge/IsoLepTagged.eL.pR_${isolep}_TRP_before.root ${dir}/TrashRecoProcessor_out/before_vtx_recovery/*.root
+#	hadd -f ${dir}/TrashRecoProcessor_out/after_vtx_recovery/root_merge/IsoLepTagged.eL.pR_${isolep}_TRP_after.root ${dir}/TrashRecoProcessor_out/after_vtx_recovery/*.root
+#	hadd -f ${dir}/particle_tagger_out/root_merge/IsoLepTagged.eL.pR_${isolep}_PT.root ${dir}/particle_tagger_out/*.root
+#	hadd -f ${dir}/truth_vertex_finder_out/root_merge/IsoLepTagged.eL.pR_${isolep}_TVF.root ${dir}/truth_vertex_finder_out/*.root
+#	hadd -f ${dir}/vertex_restorer_out/root_merge/IsoLepTagged.eL.pR_${isolep}_VR.root ${dir}/vertex_restorer_out/*.root
+fi
 
